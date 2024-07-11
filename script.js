e0f15ec751bb1e7d9eb709f13604af3d4cc78fb4
JobLessGod <iamjoblessgod@gmail.com>
2024-07-11 11:27:22 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// alert("Script JS Conntected!")
+
+// this function fetch subs count from youtube
+
+function GetSubsCount() {
+  const SubCount = document.getElementById("SubCount");
+  fetch(
+    "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCX6OQ3DkcsbYNE6H8uQQuVA&key=AIzaSyDHQdTvHNHdpyOxkLizf7zcUKng0jBNQHM"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+    //   console.log(data);
+      console.log(`SUBS: ${data.items[0].statistics.subscriberCount}`);
+      SubCount.innerHTML = data.items[0].statistics.subscriberCount
+    });
+}
+
+onload = GetSubsCount;
