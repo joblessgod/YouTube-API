ec0c07a0f95a83cd6ef4f8c0f4c71da50e38f0f6
JobLessGod <iamjoblessgod@gmail.com>
2024-08-18 23:20:48 +0545

    added own channel

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function GetSubsCount() {
 }
 
 onload = GetSubsCount;
+
+function GetJLGSubsCount() {
+  const JLGSubCount = document.getElementById("JLGSubCount");
+  fetch(
+    "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCnY0-WdawRCyZ_Mli7UUMaw&key=AIzaSyDHQdTvHNHdpyOxkLizf7zcUKng0jBNQHM"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+    //   console.log(data);
+      console.log(`SUBS: ${data.items[0].statistics.subscriberCount}`);
+      JLGSubCount.innerHTML = data.items[0].statistics.subscriberCount
+    });
+}
+
+onload = GetJLGSubsCount;
